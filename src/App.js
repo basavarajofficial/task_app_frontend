f875a16840730d6bda6b761f177b5ba1d457fbf5
basavarajofficial <bmannangi34@gmail.com>
2023-11-23 23:56:03 +0530

    added new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Main from './components/Main';
 import NewMain from './components/NewMain';
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> */}
       <NewMain />
     </div>
   );
--- src/components/apiHandler.js
+++ src/components/apiHandler.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URI = "http://localhost:5000/todos";
+
+const BASE_URI=process.env.BASE_URL || "http://localhost:5000/todos";
 
 //get todos
 export const getAllTodos = async() => {
